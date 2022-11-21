fbbc5ad4add51ac73f3358b76eb5a9419a83987e
jannyboy313 <janvanoverbeek12@gmail.com>
2022-11-21 21:23:04 +0100

    Split the table in two

--- docker/db_setup.sql
+++ docker/db_setup.sql
@@ -2,5 +2,14 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE quotes (
     id uuid NOT NULL DEFAULT uuid_generate_v4(),
     quote TEXT NOT NULL,
-    name TEXT NOT NULL
+    name_id uuid NOT NULL,
+    CONSTRAINT fk_name
+        FOREIGN KEY(name_id)
+            REFERENCES names(id)
+)
+
+CREATE TABLE names (
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    PRIMARY KEY(id)
 )
